5ff9591897406dddcd100cea4d01b2ae548bef07
Arttu Pennanen <arttu@pennanen.org>
2021-12-08 22:19:55 +0200

    Updated moti-messages

--- app/src/main/java/com/example/vesihiisi/MotivationMessages.java
+++ app/src/main/java/com/example/vesihiisi/MotivationMessages.java
@@ -1,6 +1,8 @@
 
 package com.example.vesihiisi;
 
+import static com.example.vesihiisi.Utilities.randomFromArrayList;
+
 import java.util.ArrayList;
 
 /**
@@ -15,10 +17,10 @@ public class MotivationMessages {
         messages.add("Jos on kuuma, ylitä päivittäinen tavoitteesi!");
         messages.add("Ottaisit bissen sijasta vettä!");
         messages.add("Muista nauttia vettä myös urheillessasi");
-        messages.add("Peruskuntoiluun pari dl vettä vartin välein");
+        messages.add("Kuntoiluun huikka vettä vartin välein");
         messages.add("Ei vettä, ei liikuntaa");
         messages.add("Mt. Everistin valloittamiseen tarvitsee vähintään 100l vettä");
-        messages.add("Jokaisen nesteen tarve on yksilöllinen");
+        messages.add("Nesteen tarve on yksilöllinen");
         messages.add("Kipeänä veden tarve korostuu");
         messages.add("Myös ruuasta saa vettä");
         messages.add("Juo vettä t: vesihiisi");
@@ -29,7 +31,6 @@ public class MotivationMessages {
      * @return a random motivational message from pre-defined messages.
      */
     public String getRandomMessage() {
-        int index = (int)(Math.random() * messages.size());
-        return messages.get(index);
+        return randomFromArrayList(this.messages);
     }
 }
