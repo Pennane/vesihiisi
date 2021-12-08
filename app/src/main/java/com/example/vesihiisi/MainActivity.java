f6401596064d20aa882468d80d183409dd9fd405
adnanavni <adnan.avni@metropolia.fi>
2021-12-08 12:01:54 +0200

    Counter class and buttons and saving added to main

--- app/src/main/java/com/example/vesihiisi/MainActivity.java
+++ app/src/main/java/com/example/vesihiisi/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.vesihiisi;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends NavigationBarActivity {
+    Counter waterCounter;
+    TextView targetConsumption, consumption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,16 +19,47 @@ public class MainActivity extends NavigationBarActivity {
 
         // Redirect the user to settings if the settings are invalid or unset
         tryRedirectToSettings();
+
+        waterCounter = new Counter(Global.read("weight", 0), Global.read("age", 0));
+        targetConsumption = findViewById(R.id.textView2);
+
+        targetConsumption.setText(Double.toString(waterCounter.getTargetConsumption()) + "ml");
+
     }
+
+    public void updateValue() {
+        consumption = findViewById(R.id.textView);
+        consumption.setText(Double.toString(waterCounter.getConsume()) + "ml");
+
+        Global.write("consumption", (int) waterCounter.getConsume());
+    }
+
+    public void glass(View view) {
+        waterCounter.consume(250);
+        updateValue();
+    }
+
+    public void bottle(View view) {
+        waterCounter.consume(500);
+        updateValue();
+    }
+
+    public void jug(View view) {
+        waterCounter.consume(1000);
+        updateValue();
+    }
+
 
     @Override
     protected void onResume() {
         super.onResume();
         tryRedirectToSettings();
+        waterCounter.setConsumption(Global.read("consumption", 0));
+        updateValue();
     }
 
     private void tryRedirectToSettings() {
-        if (!Global.isValidAge(Global.read("age", -1)) || !Global.isValidWeight(Global.read("weight", -1)) || !Global.isValidGender(Global.read("gender", "invalid")) ) {
+        if (!Global.isValidAge(Global.read("age", -1)) || !Global.isValidWeight(Global.read("weight", -1)) || !Global.isValidGender(Global.read("gender", "invalid"))) {
             Intent newActivity = new Intent(this, SettingsActivity.class);
             startActivity(newActivity);
         }
