85db394a854cc53d9c4a3d5c7b01e4445f3671a7
Arttu Pennanen <arttu@pennanen.org>
2021-12-08 12:37:02 +0200

    Reapply counter to mainactivity

--- app/src/main/java/com/example/vesihiisi/MainActivity.java
+++ app/src/main/java/com/example/vesihiisi/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.vesihiisi;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends NavigationBarActivity {
+    Counter waterCounter;
+    TextView targetConsumption, consumption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,18 +22,70 @@ public class MainActivity extends NavigationBarActivity {
 
         // Redirect the user to settings if the settings are invalid or unset
         tryRedirectToSettings();
+        consumeWaterAlarmSchedule();
+
+        waterCounter = new Counter(Global.read("weight", 0), Global.read("age", 0));
+        targetConsumption = findViewById(R.id.textView2);
+        targetConsumption.setText(Double.toString(waterCounter.getTargetConsumption()) + "ml");
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         tryRedirectToSettings();
+        waterCounter.setConsumption(Global.read("consumption", 0));
+        updateConsumptionViewValue();
     }
 
+
+    public void updateConsumptionViewValue() {
+        consumption = findViewById(R.id.textView);
+        consumption.setText(Double.toString(waterCounter.getConsume()) + "ml");
+        Global.write("consumption", (int) waterCounter.getConsume());
+    }
+
+    public void onGlassButtonClick(View view) {
+        waterCounter.consume(250);
+        updateConsumptionViewValue();
+    }
+
+    public void onBottleButtonClick(View view) {
+        waterCounter.consume(500);
+        updateConsumptionViewValue();
+    }
+
+    public void onJugButtonClick(View view) {
+        waterCounter.consume(1000);
+        updateConsumptionViewValue();
+    }
+
+    /**
+     * Method that redirects the current activity to settings if saved preferences are invalid
+     */
     private void tryRedirectToSettings() {
-        if (!Global.isValidAge(Global.read("age", -1)) || !Global.isValidWeight(Global.read("weight", -1)) || !Global.isValidGender(Global.read("gender", "invalid")) ) {
+        if (!Global.isValidAge(Global.read("age", -1)) || !Global.isValidWeight(Global.read("weight", -1)) || !Global.isValidGender(Global.read("gender", "invalid"))) {
             Intent newActivity = new Intent(this, SettingsActivity.class);
             startActivity(newActivity);
         }
+    }
+
+    /**
+     * Schedule alarm for water consumption
+     */
+    public void consumeWaterAlarmSchedule() {
+        Intent intent = new Intent(getApplicationContext(), NotificationHandler.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        if (alarmManager == null) {
+            return;
+        }
+
+        // Schedule alarm avery 3 minutes for testing purposes
+        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime(),
+                60 * 1000 * 3,
+                pendingIntent);
     }
 }
